8c151167899650349fe33c46c0e55c855cd807cc
Charan Ramachandran <charan@MacBook-Pro.local>
2020-10-01 13:49:36 -0400

    second

--- app.py
+++ app.py
@@ -3,9 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def index():
 	return render_template('index.html')
 
-@app.route("/login.html")
+@app.route("/login")
 def login():
 	return render_template('login.html')
+
+@app.route("/books")
+def books():
+	return render_template('books.html')
+
+if __name__ == "__main__":
+	app.run()
